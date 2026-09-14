49fbd81479f614da1cfb4b0d918fd71cf418712e
John McGill <john.mcgill@orvia.org.uk>
2026-09-14 01:49:46 +0100

    Load Production OS styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import './light-overrides.css';
+import './production.css';
 
 export const metadata: Metadata = {
   title: 'ORVIA Command',
